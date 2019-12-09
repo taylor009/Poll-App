3f1e260529ee0e7c4d33ec03dcd5e8677be80b28
Taylor Gagne <tgagne@netsurion.com>
2019-12-09 17:32:42 -0500

    finished up app.

--- polls/tests.py
+++ polls/tests.py
@@ -1,13 +1,22 @@
 import datetime
 
-from django.test import TestCase
-from django.utils import timezone
 from django.urls import reverse
+from django.utils import timezone
+from django.test import TestCase
 
 from .models import Question
 
 
 class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
         """
@@ -17,7 +26,7 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
-
+    
     def test_was_published_recently_with_recent_question(self):
         """
         was_published_recently() returns True for questions whose pub_date
@@ -27,21 +36,22 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
-    def create_question(question_text, days):
-        """
-        Create a question with the given `question_text` and published the
-        given number of `days` offset to now (negative for questions published
-        in the past, positive for questions that have yet to be published).
-        """
-        time = timezone.now() + datetime.timedelta(days=days)
-        return Question.objects.create(question_text=question_text, pub_date=time)
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
-            If no questions exist, an appropriate message is displayed.
-            """
+        If no questions exist, an appropriate message is displayed.
+        """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
@@ -49,9 +59,9 @@ class QuestionIndexViewTests(TestCase):
 
     def test_past_question(self):
         """
-            Questions with a pub_date in the past are displayed on the
-            index page.
-            """
+        Questions with a pub_date in the past are displayed on the
+        index page.
+        """
         create_question(question_text="Past question.", days=-30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
@@ -61,9 +71,9 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_question(self):
         """
-            Questions with a pub_date in the future aren't displayed on
-            the index page.
-            """
+        Questions with a pub_date in the future aren't displayed on
+        the index page.
+        """
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.")
@@ -71,9 +81,9 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_question_and_past_question(self):
         """
-            Even if both past and future questions exist, only past questions
-            are displayed.
-            """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        """
         create_question(question_text="Past question.", days=-30)
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
@@ -84,8 +94,8 @@ class QuestionIndexViewTests(TestCase):
 
     def test_two_past_questions(self):
         """
-            The questions index page may display multiple questions.
-            """
+        The questions index page may display multiple questions.
+        """
         create_question(question_text="Past question 1.", days=-30)
         create_question(question_text="Past question 2.", days=-5)
         response = self.client.get(reverse('polls:index'))
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.views import generic
 from django.utils import timezone
+from django.views import generic
 
 from .models import Choice, Question
 
@@ -12,8 +12,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -49,17 +54,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
-
-
-
-
